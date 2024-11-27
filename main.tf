c6dbaf82a9764c6919c46a0958585f3900df0b49
Benjamin Ghosez <71718181+BenjamssOdisee@users.noreply.github.com>
2024-11-27 13:56:06 +0100

    fjej

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "google_storage_bucket" "cicd-bucket" {
- name = "tt-cicd-bucket"
- location = "US"
- force_destroy = true
- public_access_prevention = "enforced"
+  name                     = "tt-cicd-bucket"
+  project                  = "CICD-dev-01-Benjamin"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
 }
